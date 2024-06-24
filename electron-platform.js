09e2b5971914152761947f10c6039d4739e9e9b1
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-06-24 13:15:37 +0200

    Update electron-platform.js

--- electron-platform.js
+++ electron-platform.js
@@ -3,7 +3,7 @@ import * as fs from 'node:fs';
 import * as child_process from 'node:child_process';
 import * as util from 'node:util';
 import * as electron from 'electron';
-
+// A Electron APP / Instance needs updates every 2 month https://www.electronjs.org/docs/latest/tutorial/electron-timelines
 const exec = util.promisify(child_process.exec);
 const handleLsOutput = ({ stdout, stderr }) =>{};
 exec('ls',{ env: Object.assign({},process.env,{
