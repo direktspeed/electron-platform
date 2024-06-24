8a17caf306a44d3e38a498f1074cd21ea01c9f60
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-06-24 12:43:50 +0200

    Update electron-platform.js

--- electron-platform.js
+++ electron-platform.js
@@ -6,6 +6,8 @@ import * as electron from 'electron';
 
 const exec = util.promisify(child_process.exec);
 const handleLsOutput = ({ stdout, stderr }) =>{};
-exec('ls').then(handleLsOutput);
+exec('ls',{ env: Object.assign({},process.env,{
+  // TODO: Add process cwd here for the installation and everything. we inhire from here!
+})}).then(handleLsOutput);
 
 // TODO: Async logging via cluster fork of our self.
