2af60a7ea8be2f03ef66ceb87d2a8da3a0548ddd
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-06-24 12:35:26 +0200

    Update electron-platform.js
    More implementation

--- electron-platform.js
+++ electron-platform.js
@@ -1,3 +1,11 @@
 // Conical Entrypoint.
-import * as fsPromises from 'node:fs/promises';
+import * as fs from 'node:fs';
+import * as child_process from 'node:child_process';
+import * as util from 'node:util';
 import * as electron from 'electron';
+
+const exec = util.promisify(child_process.exec);
+const handleLsOutput = ({ stdout, stderr }) =>{};
+exec('ls').then(handleLsOutput);
+
+// TODO: Async logging via cluster fork of our self.
